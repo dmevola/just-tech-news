da9d28c6d07648e886dbe0fb3b9157a0c502c10b
dmevola <dmevola@gmail.com>
2022-02-22 22:23:50 -0500

    hashed passwords

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 //create user model
 class User extends Model {}
@@ -44,6 +45,17 @@ User.init(
         }
         },
     {
+        hooks: {
+            //set up beforeCreate lifecycle 'hook' functionality
+           async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
         //table column options go here
 
         //pass in our imported sequelize connection (the direct connect to db)
