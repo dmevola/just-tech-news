00282c75bcf5557501f79dd8d46210ebf59610fc
dmevola <dmevola@gmail.com>
2022-03-06 19:22:48 -0500

    finished auth

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { User } = require('../../models');
+const bcrypt = require('bcrypt');
 
 //GET /api/users
 router.get('/', (req, res) => {
@@ -49,6 +50,30 @@ router.post('/', (req, res) => {
         res.status(500).json(err);
     })
 });
+
+//login route
+router.post('/login', (req, res) => {
+  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  }).then(dbUserData => {
+    if (!dbUserData) {
+      res.status(400).json({ message: 'No user with that email address!' });
+      return;
+    }
+
+    const validPassword = dbUserData.checkPassword(req.body.password);
+    if (!validPassword) {
+      res.status(400).json({ message: 'Incorrect password!' });
+      return;
+    }
+
+    res.json({ user: dbUserData, message: 'You are now logged in!' });
+  });
+});
+
 
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
